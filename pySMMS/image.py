676d00141053d84ccb3527603a8a2d9ae4d3c5c0
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-06-19 06:25:19 +0800

    implement image uploading utils

---
+++ pySMMS/image.py
@@ -0,0 +1,39 @@
+from requests import get, post
+from orjson import loads
+
+
+def clear_temp_upload_history(token):
+    return loads(get(
+        "https://sm.ms/api/v2/clear",
+        headers={"Authorization": token}
+    ).content)
+
+
+def get_temp_upload_history(token):
+    return loads(get(
+        "https://sm.ms/api/v2/history",
+        headers={"Authorization": token}
+    ).content)
+
+
+def get_upload_history(token, page):
+    return loads(get(
+        f"https://sm.ms/api/v2/history?{page=}",
+        headers={"Authorization": token}
+    ).content)
+
+
+# noinspection PyShadowingBuiltins
+def delete_image(token, hash):
+    return loads(get(
+        f"https://sm.ms/api/v2/delete/{hash}",
+        headers={"Authorization": token}
+    ).content)
+
+
+def upload_image(token, file):
+    return loads(post(
+        "https://sm.ms/api/v2/upload",
+        {"smfile": file},
+        headers={"Authorization": token}
+    ).content)
