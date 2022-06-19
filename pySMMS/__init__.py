4d4e84402db107ad867d850a8cdcd31077bf18ab
Muspi Merol <74518716+CNSeniorious000@users.noreply.github.com>
2022-06-20 07:50:19 +0800

    🎉 upload to PyPI

--- pySMMS/__init__.py
+++ pySMMS/__init__.py
@@ -1,1 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.1.0"
+
+from .user import User
+from .image import *
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='SM.MS',
+    version='0.1.0',
+    packages=['pySMMS'],
+    url='https://github.com/CNSeniorious000/SMMS4py',
+    license='MIT License',
+    author='MuspiMerol',
+    author_email='wjj123345567@iCloud.com',
+    description='unofficial python SDK for SM.MS',
+    long_description=open("readme.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=open("requirements.txt", encoding="utf-8").read()
+)
